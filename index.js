980073791e39b8c7c4142abbf91eebbb16baa8f1
HenokG7 <henoktesfayeg7@gmail.com>
2024-09-13 00:44:28 +1200

    second project

--- index.js
+++ index.js
@@ -18,33 +18,38 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+// Second challenge
+app.get("/api/whoami", (req, res) => {
+  let ip = req.ip;
+  let lang = req.headers["accept-language"];
+  let software = req.headers["user-agent"];
+
+  res.json({ ipaddress: ip, language: lang, software: software });
 });
-app.get("/api/:date?", function (req, res) {
-  console.log(req.params.date);
-  if (req.params.date != undefined) {
-    if (req.params.date.length == 13) {
-      //unix is given
-      res.json({
-        unix: parseInt(req.params.date),
-        utc: new Date(parseInt(req.params.date)).toUTCString(),
-      });
-    } else {
-      if (!isNaN(Date.parse(req.params.date))) {
-        let unix = new Date(req.params.date).getTime();
-        res.json({ unix: unix, utc: new Date(parseInt(unix)).toUTCString() });
-      } else {
-        console.log("error");
-        res.json({ error: "Invalid Date" });
-      }
-    }
-  } else {
-    let unix = Date.now();
-    res.json({ unix: unix, utc: new Date().toUTCString() });
-  }
-});
+//first challenge
+// app.get("/api/:date?", function (req, res) {
+//   console.log(req.params.date);
+//   if (req.params.date != undefined) {
+//     if (req.params.date.length == 13) {
+//       //unix is given
+//       res.json({
+//         unix: parseInt(req.params.date),
+//         utc: new Date(parseInt(req.params.date)).toUTCString(),
+//       });
+//     } else {
+//       if (!isNaN(Date.parse(req.params.date))) {
+//         let unix = new Date(req.params.date).getTime();
+//         res.json({ unix: unix, utc: new Date(parseInt(unix)).toUTCString() });
+//       } else {
+//         console.log("error");
+//         res.json({ error: "Invalid Date" });
+//       }
+//     }
+//   } else {
+//     let unix = Date.now();
+//     res.json({ unix: unix, utc: new Date().toUTCString() });
+//   }
+// });
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
